799802da9f0929c2752f6b115e59293457d3a64f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-06-25 17:50:16 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,4 +3,7 @@ attr_accessor :title, :author, :page_count, :genre
   def initialize(title)
     @title = title
   end
+  def turn_page
+     puts "Flipping the page...wow, you read fast!"
+   end
 end
